279cea09c93e532b5478e4382df963d4a1b8fefe
DARSHAN V G <darshanguna22@gmail.com>
2025-12-29 18:20:36 +0530

    done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    allowedHosts: ['hostelinfinitum.psgtech.ac.in'],
+  },
   preview: {
+    host: true,
     allowedHosts: ['hostelinfinitum.psgtech.ac.in'],
   },
 })
